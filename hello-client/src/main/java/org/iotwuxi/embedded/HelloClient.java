7b109452702b86f79aedbb4fb2bfb771f3b6f886
xukai <xukai19871105@126.com>
2019-07-07 21:13:02 +0800

    去除POM文件版本编号

--- hello-client/src/main/java/org/iotwuxi/embedded/HelloClient.java
+++ hello-client/src/main/java/org/iotwuxi/embedded/HelloClient.java
@@ -7,32 +7,26 @@ import java.net.URISyntaxException;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
 import org.eclipse.californium.core.Utils;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.elements.exception.ConnectorException;
 
 /**
  * @author xukai
  */
 public class HelloClient {
+    // private static final String serverUrl = "coap://127.0.0.1:5683/time";
+    private static final String serverUrl = "coap://139.196.187.107:5683/test";
     public static void main(String[] args) throws IOException, ConnectorException {
-        URI uri = null;
 
-        if (args.length > 0) {
-            try {
-                uri = new URI(args[0]);
-            } catch (URISyntaxException e) {
-                System.err.println("Invalid URI: " + e.getMessage());
-                System.exit(-1);
-            }
-        } else {
-            try {
-                uri = new URI("coap://127.0.0.1/time");
-            } catch (URISyntaxException e) {
-                System.err.println("Invalid URI: " + e.getMessage());
-                System.exit(-1);
-            }
-        }
+        NetworkConfig networkConfig = NetworkConfig.createStandardWithoutFile();
+        CoapEndpoint.Builder coapEndpointBuilder = new CoapEndpoint.Builder();
+        coapEndpointBuilder.setNetworkConfig(networkConfig);
 
-        CoapClient client = new CoapClient(uri);
+        CoapClient client = new CoapClient();
+        client.setEndpoint(coapEndpointBuilder.build());
+        client.setURI(serverUrl);
+
         CoapResponse response = client.get();
 
         if (response != null) {
